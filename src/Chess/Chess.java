0f6756d9a71de50e6bb25531952249c4b52a5b20
Black Rose <desertblackeage@gmail.com>
2013-10-01 21:21:14 +0800

    delete chess.java

--- src/Chess/Chess.java
+++
@@ -1,9 +0,0 @@
-package Chess;
-public interface Chess {
-	String chessName = "";			//棋子名稱
-	String[] eatRule = {};			//可吃的棋子
-	int[] bordPosition = {};		//棋盤位置
-	boolean chessCover = false;		//是否為覆蓋的(暗棋使用)
-	
-	public boolean moveRule();		//移動的規則
-}
