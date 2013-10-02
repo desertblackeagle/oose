fe2f8b9d570ecc182f84616d55a62782a73c231e
Black Rose <desertblackeage@gmail.com>
2013-10-02 19:56:06 +0800

    new a example.java for example 新建立一個範本java

---
+++ src/Chess/example.java
@@ -0,0 +1,15 @@
+package Chess;
+
+/* Author:Red Rose
+ * Function:a example
+ * Version:1.0
+ * Last Update:2013/10/2
+ */
+public class example {
+	
+	int exampleFortest ;	//a example of variable
+	
+	public void exampleMethod(){
+		System.out.println("example");	//print example
+	}
+}
