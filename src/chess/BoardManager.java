233ec1c7595b787067e4c144b51deab49a27b80d
Black Rose <desertblackeage@gmail.com>
2013-10-07 17:11:38 +0800

    實作BoardManager裡moveTo方法的虛擬碼

--- src/chess/BoardManager.java
+++ src/chess/BoardManager.java
@@ -1,5 +1,46 @@
 package chess;
 
-public class BoardManager {
+/* @author Red Rose
+ * Function:a manager to control board
+ * @ version 1.0
+ * Last Update:2013/10/7
+ */
 
+public class BoardManager {
+	
+	public void moveTo(){
+		moveChess();
+		if(canMove()){
+			if(haveTarget()){
+				if(canEat()){
+					eatChess();
+				}
+				else{
+					//move back
+		   		}
+			}
+		}else{
+			//move back
+		}
+	}
+	
+	private void moveChess(){
+		
+	}
+	
+	private boolean canMove(){
+		return true;
+	}
+	
+	private boolean haveTarget(){
+		return true;
+	}
+	
+	private boolean canEat(){
+		return true;
+	}
+	
+	private void eatChess(){
+		
+	}
 }
--- src/chess/Example.java
+++ src/chess/Example.java
@@ -5,6 +5,7 @@ package chess;
  * Version:1.0
  * Last Update:2013/10/2
  */
+
 public class Example {
 	
 	int exampleFortest ;	//a example of variable
