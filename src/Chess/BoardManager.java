66a40774c6193a2639cb7d988379a778bb4eb727
Black Rose <desertblackeage@gmail.com>
2013-10-19 23:28:46 +0800

    完成moveTo部分的實作 剩餘部分 1.跟swing有關部分 2.台灣象棋有關 內含測試程式碼

--- src/Chess/BoardManager.java
+++ src/Chess/BoardManager.java
@@ -2,41 +2,73 @@ package Chess;
 
 /* @author Red Rose
  * Function:a manager to control board
- * @ version 1.0
- * Last Update:2013/10/7
+ * @ version 1.1
+ * Last Update:2013/10/18
  */
 
 public class BoardManager {
-	
-	public void moveTo(){
-		//(move chess to position)
-		if(canMove()){
-			if(haveTarget()){
-				if(canEat()){
-					eatChess();
-				}
-				else{
-					//(move back)
-		   		}
+
+	final static int turnRed = 1;
+	final static int turnBlack = 0;
+
+	int forWhoToChess = 1;
+	int whichGame = 1; // chinese chess = 1 ; taiwanese chess = 0;
+
+	Chess[][] chineseChessLocationList = new Chess[10][9];
+
+	public void moveTo(Chess chess, int toX, int toY) {
+		// (move chess to position)
+		if (canMove(chess, toX, toY)) {
+			if (haveTarget(toX, toY)) {
+				eatChess(chess, toX, toY);
+			} else {
+				chineseChessLocationList[toX][toY] = chineseChessLocationList[chess.x][chess.y];
+				chineseChessLocationList[chess.x][chess.y] = null;
 			}
-		}else{
-			//(move back)
+		} else {
+			// (move back)
 		}
 	}
-	
-	private boolean canMove(){
+
+	private boolean canMove(Chess chess, int toX, int toY) {
+		return chess.moveRule();
+	}
+
+	private boolean haveTarget(int toX, int toY) {
+		if (chineseChessLocationList[toX][toY] == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// only for taiwanese use
+	private boolean canEat() {
+
 		return true;
 	}
-	
-	private boolean haveTarget(){
-		return true;
+
+	private void eatChess(Chess chess, int toX, int toY) {
+		// the chess from chessLocationList1[toX][toY] been set location to out of board
+		chineseChessLocationList[toX][toY] = chineseChessLocationList[chess.x][chess.y];
+		chineseChessLocationList[chess.x][chess.y] = null;
 	}
-	
-	private boolean canEat(){
-		return true;
+
+	public void printChess() {
+		System.out.println();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (this.chineseChessLocationList[i][j] == null) {
+					System.out.printf("%7s", "null");
+				} else {
+					System.out.printf("%7s", this.chineseChessLocationList[i][j].name);
+				}
+			}
+			System.out.println();
+		}
 	}
-	
-	private void eatChess(){
-		
+
+	public static void main(String[] args) {
+		System.out.println();
 	}
 }
