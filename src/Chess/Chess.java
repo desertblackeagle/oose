ff41475fca4fdb6ac16e11894e610e4df90ba900
User <User@User-PC>
2013-10-20 20:42:53 +0800

    mutator and accessor methods

--- src/Chess/Chess.java
+++ src/Chess/Chess.java
@@ -8,16 +8,60 @@ public class  Chess {
 	private int color = 0;// 0 為 紅色 , 1 為黑色;
 	private String[] eatRule = {};
 
-	public Chess()
-	{
-		
-	}
-	public Chess(String name )
-	{
-		chessName = name;
-	}
+//	public Chess()
+//	{
+//		
+//	}
+//	public Chess(String name )
+//	{
+//		chessName = name;
+//	}
 	
-	
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+	public String getName()
+	{
+		return chessName;
+	}
+	public boolean getChessCover()
+	{
+		return chessCover;
+	}
+	public void setChessCover(boolean cover)
+	{
+		chessCover = cover;
+	}
+	public int getColor()
+	{
+		return color;
+	}
+	public void setColor(int color)
+	{
+		this.color = color;
+	}
+	public String[] getEatRule()
+	{
+		String[] temp = new String[eatRule.length];
+		for(int i=0 ; i < eatRule.length ; i++)
+		{
+			temp[i] = eatRule[i];
+		}
+		return temp;
+	}
 	public boolean moveRule()
 	{
 		
