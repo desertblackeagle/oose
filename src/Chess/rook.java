47fb96352cc01397d2b81f37326104a895990f73
User <User@User-PC>
2013-10-20 22:11:59 +0800

    rook

---
+++ src/Chess/rook.java
@@ -0,0 +1,11 @@
+package Chess;
+
+public class rook extends Chess{
+	
+	public rook(int color , boolean chessCover , int x ,int y)
+	{
+		super(color, chessCover, x, y);
+		setName("Rook");
+	}
+
+}
