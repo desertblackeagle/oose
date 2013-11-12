163a1734e84c39a640f9e0635f0c290577c2a014
Black Rose <desertblackeage@gmail.com>
2013-11-12 21:15:27 +0800

    FQQ

--- src/controlUnit/ChessRecord.java
+++
@@ -1,9 +0,0 @@
-package controlUnit;
-
-import chess.Chess;
-
-public class ChessRecord {
-	public void record(Chess chess, int toX, int toY) {
-		System.out.println(chess.getName() +" "+ chess.getX() +" "+ chess.getY() +" "+ toX +" "+ toY );
-	}
-}
