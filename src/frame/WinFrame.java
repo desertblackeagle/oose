b2ce6b8dfb8ad63d8719702271ad5cb26080b1b3
rose <rose@192.168.1.202>
2013-11-20 19:12:57 +0800

    ?

--- src/frame/WinFrame.java
+++ src/frame/WinFrame.java
@@ -1,15 +1,27 @@
 package frame;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 
 public class WinFrame {
 	public WinFrame() {
 		// TODO Auto-generated constructor stub
-		JFrame winFrame = new JFrame("Win Frame");
+		JFrame winFrame = new JFrame();
+		winFrame.setUndecorated(true);
 		winFrame.setVisible(true);
 		winFrame.setSize(300, 300);
 		winFrame.setLocation(100, 100);
+		winFrame.setBackground(Color.black);
+//		try {
+//			winFrame.wait(3000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		winFrame.dispose();
+//		winFrame.setUndecorated(false);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
