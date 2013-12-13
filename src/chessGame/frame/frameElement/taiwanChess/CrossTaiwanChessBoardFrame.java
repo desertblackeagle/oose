8bf71e5e94ea6897ad8abda65d16dcdc8c38f8f5
rose <rose@192.168.1.202>
2013-12-13 21:49:37 +0800

    debug

--- src/chessGame/frame/frameElement/taiwanChess/CrossTaiwanChessBoardFrame.java
+++ src/chessGame/frame/frameElement/taiwanChess/CrossTaiwanChessBoardFrame.java
@@ -4,10 +4,14 @@ import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import chessGame.frame.dialog.FrameCheckDialog;
 
 /* @author Red Rose
  * Function:a element of taiwan chess board
@@ -32,32 +36,33 @@ public class CrossTaiwanChessBoardFrame extends JPanel {
 		float lineWidth = 3.25f;
 		((Graphics2D) g).setStroke(new BasicStroke(lineWidth));
 
-		for (int i = 0; i < 5; i++) {
-			((Graphics2D) g).drawLine(30, 30 + i * 70, 590, 30 + i * 70);
-		}
-
 		for (int i = 0; i < 9; i++) {
-			((Graphics2D) g).drawLine(30 + i * 70, 30, 30 + i * 70, 310);
+			((Graphics2D) g).drawLine(30, 30 + i * 70, 310, 30 + i * 70);
+		}
+
+		for (int i = 0; i < 5; i++) {
+			((Graphics2D) g).drawLine(30 + i * 70, 30, 30 + i * 70, 590);
 		}
 
 		for (int i = 0; i < 2; i++) {
-			((Graphics2D) g).drawLine(30 + i * 560, 380, 30 + i * 560, 660);
+			((Graphics2D) g).drawLine(380 + i * 280, 30, 380 + i * 280, 590);
 		}
 
 		for (int i = 0; i < 2; i++) {
-			((Graphics2D) g).drawLine(30, 380 + i * 280, 590, 380 + i * 280);
+			((Graphics2D) g).drawLine(380, 30 + i * 560, 660, 30 + i * 560);
 		}
 
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CrossTaiwanChessBoardFrame cp = new CrossTaiwanChessBoardFrame(630, 700, "c:/aa.jpg");
+		CrossTaiwanChessBoardFrame cp = new CrossTaiwanChessBoardFrame(700, 630, "c:/aa.jpg");
 		cp.setBounds(0, 0, 800, 800);
 		JFrame frame = new JFrame();
 		frame.setLayout(null);
 		frame.setVisible(true);
 		frame.setSize(800, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(cp);
 	}
 }
